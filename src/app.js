1d5b544be4b2873b3c25a3bd880ec68e2bafeb64
Abhijeet <aabhijeet@gmail.com>
2022-01-14 08:58:26 +0530

    commented dummy expense data

--- src/app.js
+++ src/app.js
@@ -16,33 +16,33 @@ const store = configureStore();
 // console.log(store.getState());
 // console.log('moment: ', moment().valueOf());
 
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-    console.log(state.filters);
-    console.log(state.expenses);
+// store.subscribe(() => {
+//     const state = store.getState();
+//     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//     console.log(visibleExpenses);
+//     console.log(state.filters);
+//     console.log(state.expenses);
 
-});
+// });
 
 
-const expenseOne = store.dispatch(addExpense({ 
-    description: 'Water bill',
-    amount: 4500,
-    createdAt: moment().subtract(1, 'months').valueOf()
-}));
+// const expenseOne = store.dispatch(addExpense({ 
+//     description: 'Water bill',
+//     amount: 4500,
+//     createdAt: moment().subtract(1, 'months').valueOf()
+// }));
 
-const expenseTwo = store.dispatch(addExpense({ 
-    description: 'Gas bill',
-    amount: 5678,
-    createdAt: moment().valueOf()
-}));
+// const expenseTwo = store.dispatch(addExpense({ 
+//     description: 'Gas bill',
+//     amount: 5678,
+//     createdAt: moment().valueOf()
+// }));
 
-const expenseThree = store.dispatch(addExpense({ 
-    description: 'Rent',
-    amount: 109500,
-    createdAt: moment().add(1, 'months').valueOf()
-}));
+// const expenseThree = store.dispatch(addExpense({ 
+//     description: 'Rent',
+//     amount: 109500,
+//     createdAt: moment().add(1, 'months').valueOf()
+// }));
 
 // store.dispatch(setTextFilter('water'));
 

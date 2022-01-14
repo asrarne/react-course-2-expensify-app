0537fed8d31b955215dad8b2633a16229297fe6f
Abhijeet <aabhijeet@gmail.com>
2022-01-14 21:00:42 +0530

    added condition in ExpenseSummary to display text expense or expenses

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,7 +5,7 @@ import numeral from 'numeral';
 
 export const ExpensesSummary = (props) => (
     <div>
-        <p>Viewing {props.expenseCount} expenses totalling {numeral(props.expensesTotal/100).format('$0,0.00')}</p>
+        <p>Viewing {props.expenseCount} { props.expenseCount > 1 ? 'expenses' : 'expense' } totalling {numeral(props.expensesTotal/100).format('$0,0.00')}</p>
     </div>
 );
 
